11a9a89b248a5b80793895a4cd9cc2c3202a8cf3
Doctor Dink <endvsgaming@gmail.com>
2024-04-11 18:47:36 -0400

    Initial commit

---
+++ game/engine.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import lzma
+import pickle
+
+import tcod
+
+import game.entity
+import game.exceptions
+import game.game_map
+import game.input_handlers
+import game.message_log
+import game.render_functions
+
+
+class Engine:
+    game_map: game.game_map.GameMap
+    game_world: game.game_map.GameWorld
+
+    def __init__(self, player: game.entity.Actor):
+        self.message_log = game.message_log.MessageLog()
+        self.mouse_location = (0, 0)
+        self.player = player
+
+    def handle_enemy_turns(self) -> None:
+        for entity in set(self.game_map.actors) - {self.player}:
+            if entity.ai:
+                try:
+                    entity.ai.perform()
+                except game.exceptions.Impossible:
+                    pass  # Ignore impossible action exceptions from AI.
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = tcod.map.compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
+
+    def render(self, console: tcod.console.Console) -> None:
+        self.game_map.render(console)
+
+        self.message_log.render(console=console, x=21, y=45, width=40, height=5)
+
+        game.render_functions.render_bar(
+            console=console,
+            current_value=self.player.fighter.hp,
+            maximum_value=self.player.fighter.max_hp,
+            total_width=20,
+        )
+
+        game.render_functions.render_dungeon_level(
+            console=console,
+            dungeon_level=self.game_world.current_floor,
+            location=(0, 47),
+        )
+
+        game.render_functions.render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
+
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
---
+++ game/entity.py
@@ -0,0 +1,160 @@
+from __future__ import annotations
+
+from typing import Optional, Tuple, Type, TypeVar, Union
+import copy
+import math
+
+import game.components.ai
+import game.components.consumable
+import game.components.equipment
+import game.components.fighter
+import game.components.inventory
+import game.components.level
+import game.game_map
+import game.render_order
+
+T = TypeVar("T", bound="Entity")
+
+
+class Entity:
+    """
+    A generic object to represent players, enemies, items, etc.
+    """
+
+    parent: Union[game.game_map.GameMap, game.components.inventory.Inventory]
+
+    def __init__(
+        self,
+        parent: Optional[game.game_map.GameMap] = None,
+        x: int = 0,
+        y: int = 0,
+        char: str = "?",
+        color: Tuple[int, int, int] = (255, 255, 255),
+        name: str = "<Unnamed>",
+        blocks_movement: bool = False,
+        render_order: game.render_order.RenderOrder = game.render_order.RenderOrder.CORPSE,
+    ):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
+        self.render_order = render_order
+        if parent:
+            # If parent isn't provided now then it will be set later.
+            self.parent = parent
+            parent.entities.add(self)
+
+    @property
+    def gamemap(self) -> game.game_map.GameMap:
+        return self.parent.gamemap
+
+    def spawn(self: T, gamemap: game.game_map.GameMap, x: int, y: int) -> T:
+        """Spawn a copy of this instance at the given location."""
+        clone = copy.deepcopy(self)
+        clone.x = x
+        clone.y = y
+        clone.parent = gamemap
+        gamemap.entities.add(clone)
+        return clone
+
+    def place(self, x: int, y: int, gamemap: Optional[game.game_map.GameMap] = None) -> None:
+        """Place this entitiy at a new location.  Handles moving across GameMaps."""
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "parent"):  # Possibly uninitialized.
+                if self.parent is self.gamemap:
+                    self.gamemap.entities.remove(self)
+            self.parent = gamemap
+            gamemap.entities.add(self)
+
+    def distance(self, x: int, y: int) -> float:
+        """
+        Return the distance between the current entity and the given (x, y) coordinate.
+        """
+        return math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
+
+    def move(self, dx: int, dy: int) -> None:
+        # Move the entity by a given amount
+        self.x += dx
+        self.y += dy
+
+
+class Actor(Entity):
+    def __init__(
+        self,
+        *,
+        x: int = 0,
+        y: int = 0,
+        char: str = "?",
+        color: Tuple[int, int, int] = (255, 255, 255),
+        name: str = "<Unnamed>",
+        ai_cls: Type[game.components.ai.BaseAI],
+        equipment: game.components.equipment.Equipment,
+        fighter: game.components.fighter.Fighter,
+        inventory: game.components.inventory.Inventory,
+        level: game.components.level.Level,
+    ):
+        super().__init__(
+            x=x,
+            y=y,
+            char=char,
+            color=color,
+            name=name,
+            blocks_movement=True,
+            render_order=game.render_order.RenderOrder.ACTOR,
+        )
+
+        self.ai: Optional[game.components.ai.BaseAI] = ai_cls(self)
+
+        self.equipment: game.components.equipment.Equipment = equipment
+        self.equipment.parent = self
+
+        self.fighter = fighter
+        self.fighter.parent = self
+
+        self.inventory = inventory
+        self.inventory.parent = self
+
+        self.level = level
+        self.level.parent = self
+
+    @property
+    def is_alive(self) -> bool:
+        """Returns True as long as this actor can perform actions."""
+        return bool(self.ai)
+
+
+class Item(Entity):
+    def __init__(
+        self,
+        *,
+        x: int = 0,
+        y: int = 0,
+        char: str = "?",
+        color: Tuple[int, int, int] = (255, 255, 255),
+        name: str = "<Unnamed>",
+        consumable: Optional[game.components.consumable.Consumable] = None,
+        equippable: Optional[game.components.equippable.Equippable] = None,
+    ):
+        super().__init__(
+            x=x,
+            y=y,
+            char=char,
+            color=color,
+            name=name,
+            blocks_movement=False,
+            render_order=game.render_order.RenderOrder.ITEM,
+        )
+
+        self.consumable = consumable
+
+        if self.consumable:
+            self.consumable.parent = self
+
+        self.equippable = equippable
+
+        if self.equippable:
+            self.equippable.parent = self
